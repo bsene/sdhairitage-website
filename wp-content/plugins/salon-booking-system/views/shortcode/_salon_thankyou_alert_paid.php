100651d217399b8532d8bda6d51edfc0bc3ef286
Birame SENE <bsene@pm.me>
2021-10-30 05:02:15 +0200

    setting up gitignore properly

--- wp-content/plugins/salon-booking-system/views/shortcode/_salon_thankyou_alert_paid.php
+++
@@ -1,3 +0,0 @@
-<div class="sln-alert sln-alert--info sln-alert--topicon sln-alert--paid">
-    <p><?php _e('You already paid for this reservation.', 'salon-booking-system') ?></p>
-</div>
--- wp-content/plugins/salon-booking-system/views/shortcode/salon_booking_calendar/calendar_full.php
+++
@@ -1,5 +0,0 @@
-<div id="sln-salon-booking-calendar-shortcode" class="alignwide">
-    <div class="wrapper">
-	<?php include 'calendar_content.php' ?>
-    </div>
-</div>
